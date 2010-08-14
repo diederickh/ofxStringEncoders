dca173de8eb3f89c336906456ceb1c1b3c900616
roxlu <diederick@apollomedia.nl>
2010-08-14 19:37:49 +0200

    Moving from Google code

---
+++ src/html.h
@@ -0,0 +1,17 @@
+#ifndef HTMLENTITIESH
+#define HTMLENTITIESH
+#include <string>
+#include <iostream>
+#include <map>
+class HTML {
+public:
+	static void writeUTF8 (
+		std::ostream & Out,
+		unsigned int Ch
+	);
+	static void unquoteHTML(
+		std::istream & In,
+		std::ostream & Out
+	);
+};
+#endif
